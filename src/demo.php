35e440253bd88b78624406247f36d073be070251
yqm <1443261811@qq.com>
2018-10-09 20:08:04 +0800

    1

--- src/demo.php
+++ src/demo.php
@@ -6,9 +6,9 @@ header("Content-type: image/jpeg");
 
 require '../vendor/autoload.php';
 $http = new HttpClient();
-
 (new QrCode)->show('https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=2&l=M&s=3&d=72&v=4&t=0.8310633005603156&daid=164&pt_3rd_aid=0');
+//(new QrCode)->show('https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=0&l=M&s=5&d=72&v=4&t=0.1');
 return;
 
-$response = $http->request('get', 'https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=2&l=M&s=3&d=72&v=4&t=0.8310633005603156&daid=164&pt_3rd_aid=0', ['verify' => false]);
+$response = $http->request('get', 'https://ssl.ptlogin2.qq.com/ptqrshow?appid=501004106&e=2&=M&s=3&d=72&v=4&t=0.8310633005603156&daid=164&pt_3rd_aid=0', ['verify' => false]);
 echo $response->getBody();
