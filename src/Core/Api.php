6be5dc5eb58b1e1b0172e0cabb45ee5e6a42ba56
yqm <yangqm@iclockwork.com>
2018-10-11 17:48:28 +0800

    1

--- src/Core/Api.php
+++ src/Core/Api.php
@@ -10,33 +10,7 @@ class Api
 
     protected $cookies;
 
-    /**
-     * send a curl request
-     *
-     * @param $api
-     * @param $data
-     * @param string $method
-     * @return mixed
-     */
-    public static function request($api, $data, $method = 'post')
-    {
-        $client = self::getClient();
-
-        $options = [
-            'verify'  => false,
-            'timeout' => 15,
-        ];
-
-        $options = is_array($data) ? array_merge($options, ['form_params' => $data]) : array_merge($options, ['body' => $data]);
-
-        $request = ($method === 'post') ? $client->post($api, $options) : $client->get($api);
-
-        $response = $request->getBody()->getContents();
-
-        return json_decode($response, true);
-    }
-
-    public function makeRequest($method, $uri, $options = array())
+    public function get($uri, $options = array())
     {
         $client = self::getClient();
 
@@ -45,7 +19,7 @@ class Api
             'cookies' => $this->cookies,
         ], $options);
 
-        return $client->request($method, $uri, $options);
+        return $client->request('get', $uri, $options);
     }
 
     protected static function getClient()
