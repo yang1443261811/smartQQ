98238044d598f0ccb61cdb3712a48269280c5810
yang3529369110 <1443261811@qq.com>
2018-10-22 11:36:19 +0800

    1

--- src/smartQQ.php
+++ src/smartQQ.php
@@ -7,14 +7,31 @@ $config = array(
     'cookie_file'       => './cookie.txt',
     'clientid'          => 53999199,
 
-    /*
-       * 日志配置项
-       */
+    /**
+     * 日志配置项
+     */
     'log'      => [
         'level'         => 'debug',
         'permission'    => 0777,
         'system'        => $path.'log.txt', // 系统报错日志
         'message'       => $path.'log.txt', // 消息日志
+    ],
+
+    /**
+     * 缓存配置项
+     */
+    'cache' => [
+        'default' => 'file', // 缓存设置 （支持 redis 或 file）
+        'stores'  => [
+            'file' => [
+                'driver' => 'file',
+                'path'   => $path.'cache',
+            ],
+            'redis' => [
+                'driver'     => 'redis',
+                'connection' => 'default',
+            ],
+        ],
     ],
 );
 
