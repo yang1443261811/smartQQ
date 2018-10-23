e04a0479addfc288f7ee0829b5c43387227bff12
yqm <yangqm@iclockwork.com>
2018-10-23 17:38:10 +0800

    1

---
+++ src/Request/Friends.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace smartQQ\Request;
+
+/**
+ * QQ好友
+ *
+ * Class Friends
+ * @package smartQQ\Request
+ */
+class Friends
+{
+    const URL = 'http://s.web2.qq.com/api/get_user_friends2';
+
+    const REFERER = 'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1';
+
+    /**
+     * 获取QQ好友
+     *
+     * @return mixed
+     */
+    public static function get()
+    {
+        $params = [
+            'vfwebqq' => app('config')['server.vfwebqq'],
+            'hash'    => hashArgs(app('config')['server.uin'], app('config')['server.ptwebqq'])
+        ];
+        $options = ['headers' => ['Referer' => self::REFERER]];
+
+        return app('http')->post(self::URL, $params, $options);
+    }
+}
---
+++ src/Request/Myself.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace smartQQ\Request;
+
+/**
+ * Class Myself
+ * @package smartQQ\Request
+ */
+class Myself
+{
+    const URL = 'http://s.web2.qq.com/api/get_self_info2?t=0.1';
+
+    const REFERER = 'http://s.web2.qq.com/proxy.html?v=20130916001&callback=1&id=1';
+
+    /**
+     * 获取自己的QQ信息
+     *
+     * @return mixed
+     */
+    public static function get()
+    {
+        $options = ['headers' => ['Referer' => self::REFERER]];
+
+        $response = app('http')->get(self::URL, $options);
+
+        return json_decode($response, true);
+    }
+}
