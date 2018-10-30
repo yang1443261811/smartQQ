5fab09f98519bf0cde5f46aaa3818d77c2b3e46a
yqm <yangqm@iclockwork.com>
2018-10-30 10:05:57 +0800

    拉取QQ消息

--- src/Request/PollMessage.php
+++ src/Request/PollMessage.php
@@ -1,15 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: BL
- * Date: 2018/10/23
- * Time: 17:39
- */
 
 namespace smartQQ\Request;
 
-
 class PollMessage
 {
+    const URL = 'http://d1.web2.qq.com/channel/poll2';
 
+    const REFERER = 'http://d1.web2.qq.com/proxy.html?v=20151105001&callback=1&id=2';
+
+    /**
+     * 拉取QQ消息
+     *
+     * @return mixed
+     */
+    public function get()
+    {
+        $options = ['headers' => ['Referer' => self::REFERER]];
+
+        $params = [
+            'key'        => '',
+            'clientid'   => app('config')['clientid'],
+            'ptwebqq'    => app('config')['server.ptwebqq'],
+            'psessionid' => app('config')['server.psessionid'],
+        ];
+
+        return app('http')->post(self::URL, $params, $options);
+    }
 }
